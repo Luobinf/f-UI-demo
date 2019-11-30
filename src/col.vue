8fd7288496923f0d18bececbabd0a264840cdd84
luobin <1096422441@qq.com>
2019-11-30 09:45:31 +0800

    大致实现row组件和col组件

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+    :style="{paddingLeft: `${gutter/2}px`,paddingRight: `${gutter/2}px`}"
+  >
+    <div style="border: 1px solid black;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,23 +13,43 @@ export default {
   name: 'fjq-col',
   props: {
     span: {
+      type: [Number,String],
+    },
+    offset: {
       type: [Number,String]
+    }
+  },
+  data(){
+    return {
+      gutter: 0
     }
   }
 }
 </script>
 
+
+
+
 <style scoped lang="scss">
   .col{
     width: 50%;
     height: 100px;
-    background-color: blue;
-    border: 1px solid red;
+    /*background-color: skyblue;*/
+    /*border: 1px solid red;*/
+    /*margin: 0 10px;*/
+    /*padding: 0 10px;*/
     $class: col-;
     @for $n from 1 through 24{
       /* .col.col-1 .col.col-2 */
       &.#{$class}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-;
+    @for $n from 1 through 24{
+      /* .col.col-1 .col.col-2 */
+      &.#{$class}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: `${-gutter/2}px`,marginRight: `${-gutter/2}px`}">
     <slot></slot>
   </div>
 </template>
 
 <script type="text/javascript">
 export default {
-  data () {
-    return {
-
+  name: `fjq-row`,
+  props: {
+    gutter: {
+      type: [Number,String]
     }
   },
-  components: {
-
+  mounted() {
+    // console.log(this.$children)
+    // 此时子组件已经挂载到了父组件上，并且父组件挂载到了页面中，此时可以获取到css的1样式了
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
   }
 }
 </script>
