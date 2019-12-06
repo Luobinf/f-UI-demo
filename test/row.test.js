0e108068896c0db6d5fa51103d7480bacd5189f5
luobin <1096422441@qq.com>
2019-12-06 21:33:17 +0800

    添加row.vue的测试代码

--- test/row.test.js
+++ test/row.test.js
@@ -1,41 +1,58 @@
-const expect = chai.expect;
+const expect = chai.expect
 import Vue from 'vue'
 import Row from '../src/row'
 import Col from '../src/col'
 
-Vue.config.productionTip = false
-Vue.config.devtools = false
-
-describe('Row', () => {
-    //BDD 行为测试驱动
-    it('存在.', () => {
-        expect(Row).to.exist //不是falsy值就可以
+describe('Row',() => {
+    it('存在',() => {
+        expect(Row).to.exist
     })
-
-    it('接收gutter属性', (done) => {
+    it('接收justify属性', function () {
+        let oDiv = document.createElement('div')
+        document.body.append(oDiv)
+        const Constructor = Vue.extend(Row)
+        const vm = new Constructor({
+            propsData: {
+                justify: 'center'
+            }
+        }).$mount(oDiv)
+        expect(getComputedStyle(vm.$el).justifyContent).to.equal('center')
+        vm.$destroy()
+    })
+    it('接收align属性', function () {
+        let oDiv = document.createElement('div')
+        document.body.append(oDiv)
+        let Constructor = Vue.extend(Row)
+        let vm = new Constructor({
+            propsData: {
+                align: 'center'
+            }
+        }).$mount(oDiv)
+        expect(getComputedStyle(vm.$el).alignItems).to.equal('center')
+        vm.$destroy()
+    })
+    it('接收gutter属性', function (done) {
+        let oDiv = document.createElement('div')
+        document.body.append(oDiv)
         Vue.component('f-row',Row)
         Vue.component('f-col',Col)
-        const oDiv = document.createElement('div')
-        document.body.append(oDiv)
         oDiv.innerHTML = `
             <f-row gutter="20">
-                <f-col span="12"></f-col>
-                <f-col span="12"></f-col>
+                <f-col span="10" style="height: 10px;"></f-col>
+                <f-col span="14" style="height: 10px;"></f-col>
             </f-row>
         `
-        const vm = new Vue({
+        let vm = new Vue({
             el: oDiv
         })
-        // console.log(vm.$el.innerHTML)
         setTimeout(() => {
-            const row = vm.$el.querySelector('.row')
-            expect(getComputedStyle(row).marginLeft).to.equal('-10px')
-            expect(getComputedStyle(row).marginRight).to.equal('-10px')
-            const colElements = vm.$el.querySelectorAll('.col')
-            expect(window.getComputedStyle(colElements[0]).paddingRight).to.equal('10px')
-            expect(window.getComputedStyle(colElements[1]).paddingLeft).to.equal('10px')
+            let row = vm.$el.querySelector('.f-row')
+            expect(getComputedStyle(row).marginLeft).to.equal(`-10px`)
+            expect(getComputedStyle(row).marginRight).to.equal(`-10px`)
+            let colS = vm.$el.querySelectorAll('.f-col')
+            expect(getComputedStyle(colS[0]).paddingLeft).to.equal('10px')
+            expect(getComputedStyle(colS[1]).paddingRight).to.equal('10px')
             done()
-        },1500)
-        // vm.$destroy()
+        },1000)
     })
 })
