7892d2f239a3186ea1f5fd89090640038a57efce
luobin <1096422441@qq.com>
2019-12-24 17:40:11 +0800

    重构tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -24,23 +24,31 @@ export default {
       eventBus: this.eventBus
     }
   },
-  mounted() {
-    if(this.$children.length === 0) {
-      console && console.warn && console.warn(`f-tabs的子组件应该是f-tabs-head和f-tabs-body`)
-    }
-    this.$children.forEach( (vm) => {
-      if(vm.$options.name === `fTabsHead`) {
-        vm.$children.forEach( (item) => {
-          if(item.$options.name === `fTabsItem` && item.name === this.selected){
-            this.eventBus.$on(`update:selected`,(val) => {
-              //监听事件，当selected的值发生改变时，通知外界让app.js的selectedTab值也进行更新
-              this.$emit(`update:selected`,val)
-            })
-            this.eventBus.$emit(`update:selected`,this.selected,item)  //找到具体的item
-          }
-        })
+  methods: {
+    checkChildren(){
+      if(this.$children.length === 0) {
+        console && console.warn && console.warn(`f-tabs的子组件应该是f-tabs-head和f-tabs-body`)
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach( (vm) => {
+        if(vm.$options.name === `fTabsHead`) {
+          vm.$children.forEach( (item) => {
+            if(item.$options.name === `fTabsItem` && item.name === this.selected){
+              this.eventBus.$on(`update:selected`,(val) => {
+                //监听事件，当selected的值发生改变时，通知外界让app.js的selectedTab值也进行更新
+                this.$emit(`update:selected`,val)
+              })
+              this.eventBus.$emit(`update:selected`,this.selected,item)  //找到具体的item
+            }
+          })
+        }
+      })
+    }
+  },
+  mounted() {
+    this.checkChildren()
+    this.selectTab()
 
   }
 }
