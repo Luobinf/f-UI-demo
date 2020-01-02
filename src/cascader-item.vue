efa3b9c708bb63ace9976cb0828370dd630ce8eb
luobin <1096422441@qq.com>
2020-01-02 21:06:09 +0800

    创建cascader组件

---
+++ src/cascader-item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    {{sourceItem.name}}
+    <f-cascader-item
+            v-for="item in sourceItem.children"
+            v-if="sourceItem.children"
+            :sourceItem="item"
+    >
+
+    </f-cascader-item>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: `fCascaderItem`,
+  props: {
+    sourceItem: {
+      type: Object
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/cascader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="cascader">
+    <div class="trigger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <div v-for="item in source">
+        <f-cascader-item :sourceItem="item"></f-cascader-item>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+import fCascaderItem from './cascader-item'
+export default {
+  name: `fCascader`,
+  props: {
+    source: {
+      type: Array,
+      validator(value){
+        return Array.isArray(value)
+      }
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    fCascaderItem
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "var";
+  .cascader{
+
+  }
+</style>
