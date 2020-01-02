d5073ac6a63142422335f4ca910c1810b61eb625
luobin <1096422441@qq.com>
2020-01-02 17:58:05 +0800

    将变量改成scss变量

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script type="text/javascript">
-    import Icon from './icon'
+import Icon from './icon'
 export default {
     name: 'fjq-button',
     components: {
@@ -41,6 +41,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
+    $font-size: 14px;
+    $button-height: 32px;
+    $button-bg: white;
+    $button-active-bg: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
     @keyframes spin {
         0%{
             transform: rotate(0deg);
@@ -50,21 +58,21 @@ export default {
         }
     }
   .f-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
+    font-size: $font-size;
+    height: $button-height;
     padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
     display: inline-flex;
     justify-content: center;
     align-items: center;
     vertical-align: top;
     &:hover {
-      border-color: var(--border-color-hover);
+      border-color: $border-color-hover;
     }
     &:active {
-      background-color: var(--button-active-bg);
+      background-color: $button-active-bg;
     }
     &:focus {
       outline: none;
