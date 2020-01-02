31fe901b95ed48ea2367c39a45801ced9e47e2e9
luobin <1096422441@qq.com>
2020-01-02 09:01:34 +0800

    collapse-item组件代码简化

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -38,11 +38,7 @@
     },
     mounted(){
       this.eventBus && this.eventBus.$on(`update:selected`,(names) => {
-        if(names.indexOf(this.name) >= 0) {
-          this.open = true
-        }else{
-          this.open = false
-        }
+        this.open = names.indexOf(this.name) >= 0
       })
     }
   };
