013588b7de69d911e4d5cf8e6e08c111cd7d7698
luobin <1096422441@qq.com>
2020-03-15 11:30:22 +0800

    修改icon.vue组件

--- test/button.test.js
+++ test/button.test.js
@@ -6,68 +6,115 @@ Vue.config.productionTip = false
 Vue.config.devtools = false
 
 //下面it和花括号一样用来隔离作用域的，每一个it都是一个测试用例
-//第一个参数是描述的名字；第二个参数为所要测试的代码，写在了一个函数中，实际执行的函数。
+//第一个参数是测试描述的名字；第二个参数为所要测试的代码(实际执行的函数)，写在了一个函数中。
 
 //Mocha是一个单元测试框架库
 describe('Button', () => {
     //BDD 行为测试驱动 TDD测试驱动开发
     //这里描述了Button的一系列行为
-    it('测试Button存在.', () => {
-        expect(Button).to.be.ok //不是falsy值就可以
+    it('Button存在 ', () => {
+        expect(Button).to.be.ok
     })
-    it('可以设置icon.', () => {
-        const Constructor = Vue.extend(Button) //通过Vue.extend()生成一个构造类，创建一个子类。
-        const vm = new Constructor({
+
+    it('可以接收icon',() => {
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
             propsData: {
                 icon: 'settings'
             }
         }).$mount()
-        const useElement = vm.$el.querySelector('use')
+        let useElement = vm.$el.querySelector('use')
+        console.log(useElement)
         expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
         vm.$destroy()
     })
-    it('可以设置loading.', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+
+    it('可以接受loading',() => {
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
             propsData: {
-                icon: 'settings',
                 loading: true
             }
         }).$mount()
-        const useElements = vm.$el.querySelectorAll('use')
-        expect(useElements.length).to.equal(1)
-        expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+        let useElement = vm.$el.querySelector('use')
+        expect(useElement.getAttribute('xlink:href')).to.equal('#i-loading')
         vm.$destroy()
     })
-    it('icon 默认的 order 是 1', () => {
+
+    it('svg默认的order为1',() => {
         const div = document.createElement('div')
-        document.body.appendChild(div)
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        document.body.append(div)
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
             propsData: {
-                icon: 'settings',
+
             }
         }).$mount(div)
-        const icon = vm.$el.querySelector('svg')
-        expect(getComputedStyle(icon).order).to.eq('1')
+        let iconElement = vm.$el.querySelector('.icon')
+        expect(getComputedStyle('iconElement').order).to.equal(1)
         vm.$el.remove()
         vm.$destroy()
     })
-    it('设置 iconPosition 可以改变 order', () => {
+
+    it('设置icon-position为right时order为2',() => {
         const div = document.createElement('div')
-        document.body.appendChild(div)
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        document.body.append(div)
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
             propsData: {
                 icon: 'settings',
-                iconPosition: 'right'
+                'icon-position': 'right'
             }
         }).$mount(div)
-        const icon = vm.$el.querySelector('svg')
-        expect(getComputedStyle(icon).order).to.eq('2')
+        let iconElement = vm.$el.querySelector('.icon')
+        expect(getComputedStyle('iconElement').order).to.equal(2)
         vm.$el.remove()
         vm.$destroy()
     })
+
+    it('默认size为medium',() => {
+        const div = document.createElement('div')
+        document.body.append(div)
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
+            propsData: {
+            }
+        }).$mount(div)
+        let buttonElement = vm.$el.querySelector('button')
+        expect(buttonElement.classList.contains('medium')).to.equal(true)
+        vm.$el.remove()
+        vm.$destroy()
+    })
+
+    it('可以接受size,将size大小设置为small',() => {
+        const div = document.createElement('div')
+        document.body.append(div)
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
+            propsData: {
+                size: 'small'
+            }
+        }).$mount(div)
+        let buttonElement = vm.$el.querySelector('button')
+        expect(buttonElement.classList.contains('small')).to.equal(true)
+        vm.$el.remove()
+        vm.$destroy()
+    })
+
+    it('type默认值为default',() => {
+        const div = document.createElement('div')
+        document.body.append(div)
+        let Constructor = Vue.extend(Button)
+        let vm = new Constructor({
+            propsData: {
+            }
+        }).$mount(div)
+        let buttonElement = vm.$el.querySelector('button')
+        expect(buttonElement.classList.contains('default')).to.equal(true)
+        vm.$el.remove()
+        vm.$destroy()
+    })
+
     it('点击 button 触发 click 事件', () => {
         const Constructor = Vue.extend(Button)
         const vm = new Constructor({
