3c513880bd782793e29026abbac0a89c96fe4f91
luobin <1096422441@qq.com>
2020-03-22 08:48:02 +0800

    改造Popover组件并测试，且暂时跳过CSS的测试

--- src/popover.vue
+++ src/popover.vue
@@ -1,204 +1,284 @@
 <template>
   <div class="f-popover" ref="popover">
-    <div class="contentWrapper" v-if="visible" ref="contentWrapper" :class="{[`position-${position}`]:true}">
-      <slot name="content" :close="close"></slot>
-    </div>
-    <span ref="triggerWrapper" class="buttonWrapper">
-        <slot></slot>
+    <transition name="fade">
+      <div
+              class="content-wrapper"
+              ref="contentWrapper"
+              v-show="showPopover"
+              :class="[`position-${position}`]"
+              :style="{ width: `${width}px` }"
+      >
+        <div class="title" v-if="title">{{ title }}</div>
+        <div class="content">{{ content }}<slot name="content"></slot></div>
+      </div>
+    </transition>
+    <span class="trigger" ref="triggerWrapper">
+      <slot></slot>
     </span>
   </div>
 </template>
 
-
-<script>
+<script type="text/javascript">
   export default {
+    name: "f-popover",
     data() {
       return {
-        visible: true,
+        showPopover: false
       };
     },
     props: {
+      visible: {
+        type: Boolean
+      },
       position: {
         type: String,
-        default: `top`,
+        default: "top",
         validator(val) {
-          return ['top','bottom','left','right'].indexOf(val) >= 0
+          return ["top", "bottom", "left", "right"].indexOf(val) >= 0;
         }
       },
       trigger: {
         type: String,
-        default: `click`,
-        validator(val){
-          return [`click`,`hover`].indexOf(val) >= 0
+        default: "click",
+        validator(val) {
+          return ["click", "hover"].indexOf(val) >= 0;
         }
+      },
+      title: {
+        type: String
+      },
+      content: {
+        type: String
+      },
+      width: {
+        type: String,
+        default: "150"
       }
     },
-    mounted(){
-      if(this.trigger === 'click'){
-        this.$refs.popover.addEventListener('click',this.onClick)
+    watch: {
+      showPopover(value) {
+        if (value) {
+          this.$emit("show");
+        } else {
+          this.$emit("hide");
+        }
+      },
+      visible: {
+        handler(value) {
+          this.showPopover = value;
+        },
+        immediate: true
+      }
+    },
+    mounted() {
+      let popover = this.$refs.popover;
+      if (this.trigger === "click") {
+        popover.addEventListener("click", this.onClick);
       } else {
-        this.$refs.popover.addEventListener('mouseenter',this.open)
-        this.$refs.popover.addEventListener('mouseleave',this.close)
+        popover.addEventListener("mouseenter", this.open);
+        popover.addEventListener("mouseleave", this.close);
+      }
+    },
+    beforeDestroy() {
+      //在beforeDestroy时，vm实例还可用
+      let popover = this.$refs.popover;
+      if (this.trigger === "click") {
+        popover.removeEventListener("click", this.onClick);
+      } else {
+        popover.removeEventListener("mouseenter", this.open);
+        popover.removeEventListener("mouseleave", this.close);
       }
     },
     methods: {
-      positionContent () {
-        document.body.appendChild(this.$refs.contentWrapper)
-        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-        let height2 = this.$refs.contentWrapper.getBoundingClientRect().height
-        let positions = {
+      eventHandler(e) {
+        if (
+            this.$refs.contentWrapper &&
+            this.$refs.contentWrapper.contains(e.target)
+        ) {
+          return;
+        }
+        // 当点击按钮时直接退出，应该由onCLick方法去关闭popover，不应该由document来关闭
+        if (this.$refs.popover && this.$refs.popover.contains(e.target)) {
+          return;
+        }
+        this.close();
+      },
+      locateContent() {
+        let contentWrapper = this.$refs.contentWrapper;
+        document.body.append(contentWrapper);
+        let triggerWrapper = this.$refs.triggerWrapper;
+        let {
+          top,
+          left,
+          bottom,
+          right,
+          height
+        } = triggerWrapper.getBoundingClientRect(); //获取元素相对于窗口的位置信息
+        let differenceHeight =
+            contentWrapper.getBoundingClientRect().height - height;
+
+        let contentPosition = {
           top: {
-            left: left + window.scrollX,
-            top: top + window.scrollY
+            left: left + window.pageXOffset,
+            top: top + window.pageYOffset
           },
           bottom: {
-            left: left + window.scrollX,
-            top: top + height + window.scrollY
+            left: left + window.pageXOffset,
+            top: bottom + window.pageYOffset
           },
           left: {
-            left: left + window.scrollX,
-            top: top + (height-height2)/2 + window.scrollY
+            left: left + window.pageXOffset,
+            top: top + window.pageYOffset - differenceHeight / 2
           },
           right: {
-            left: left + width + window.scrollX,
-            top: top + (height-height2)/2 + window.scrollY
+            left: right + window.pageXOffset,
+            top: top + window.pageYOffset - differenceHeight / 2
           }
-        }
-        this.$refs.contentWrapper.style.left = positions[this.position].left + `px`
-        this.$refs.contentWrapper.style.top = positions[this.position].top + `px`
+        };
+        contentWrapper.style.left = `${contentPosition[this.position].left}px`;
+        contentWrapper.style.top = `${contentPosition[this.position].top}px`;
       },
-      onClickDocument (e) {
-        if (this.$refs.popover &&
-                (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
-        ) { return }
-        if (this.$refs.contentWrapper &&
-                (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
-        ) { return }
-        this.close()
+      open() {
+        this.showPopover = true;
+        this.$emit("update:visible", true);
+        setTimeout(() => {
+          this.locateContent();
+          document.addEventListener("click", this.eventHandler);
+        }, 0);
+        // 下面出了一个问题，为什么document的监听不是在下一个事件循环中
+        // this.$nextTick( () => {
+        //   this.locateContent()
+        //   document.addEventListener('click',this.eventHandler)
+        // })
       },
-      open () {
-        this.visible = true
-        this.$nextTick(() => {
-          this.positionContent()
-          document.addEventListener('click', this.onClickDocument)
-        })
+      close() {
+        this.showPopover = false;
+        this.$emit("update:visible", false);
+        document.removeEventListener("click", this.eventHandler);
       },
-      close () {
-        this.visible = false
-        document.removeEventListener('click', this.onClickDocument)
-      },
-      onClick (event) {
-        if (this.$refs.triggerWrapper.contains(event.target)) {
-          if (this.visible === true) {
-            this.close()
+      onClick(e) {
+        if (this.$refs.triggerWrapper.contains(e.target)) {
+          if (this.showPopover === true) {
+            this.close();
           } else {
-            this.open()
+            this.open();
           }
         }
-      }
-    },
-    destroyed() {
-      if(this.trigger === 'click'){
-        this.$refs.popover.removeEventListener('click',this.onClick)
-      } else {
-        this.$refs.popover.removeEventListener('mouseenter',this.open)
-        this.$refs.popover.removeEventListener('mouseleave',this.close)
       }
     }
   };
 </script>
 
 <style scoped lang="scss">
-  $border-color: #333;
   $border-radius: 4px;
+  $border-color: #333;
+  $font-size: 14px;
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+  .fade-enter-to {
+    opacity: 100%;
+  }
   .f-popover {
     display: inline-block;
     position: relative;
+    .trigger {
+      display: inline-block;
+    }
   }
-  .contentWrapper {
-    border: 1px solid $border-color;
+  .content-wrapper {
     position: absolute;
-    padding: 0.5em 1em;
+    border: 1px solid $border-color;
     border-radius: $border-radius;
-    max-width: 20em;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    /*filter: drop-shadow(0 0 3px rgba(0, 0, 0, 0.5));*/
+    background-color: white;
     word-break: break-all;
-    filter: drop-shadow(0 1px 1px rgba(0,0,0,0.5));
-    background: white;
-    &::before,&::after{
-      content: '';
+
+    .title {
+      border-bottom: 1px solid rgb(232, 232, 232);
+      padding: 0.5em 1em;
+      font-size: $font-size;
+      font-weight: 500;
+      color: rgba(0, 0, 0, 0.85);
+      max-width: 20em;
+    }
+    .content {
+      padding: 0.5em 1em;
+      font-size: $font-size;
+      font-weight: 400;
+      color: rgba(0, 0, 0, 0.65);
+      max-width: 20em;
+    }
+
+    &::before,
+    &::after {
+      content: "";
       display: block;
-      border: 10px solid transparent;
       width: 0;
       height: 0;
+      border: 5px solid transparent;
       position: absolute;
     }
-    &.position-top{
+
+    &.position-top {
       transform: translateY(-100%);
       margin-top: -10px;
-      &::before,&::after{
-        border-bottom: none;
-        left: 10px;
-      }
-      &::before{
+      &::before,
+      &::after {
         border-top-color: black;
+        left: 10%;
         top: 100%;
       }
-      &::after{
+      &::after {
         border-top-color: white;
         top: calc(100% - 1px);
       }
     }
-    &.position-bottom{
+    &.position-bottom {
       margin-top: 10px;
-      &::before,&::after{
-        border-top: none;
-        left: 10px;
-      }
-      &::before{
-        border-bottom-color: black;
+      &::before,
+      &::after {
+        left: 20%;
         bottom: 100%;
+        border-bottom-color: black;
       }
-      &::after{
+      &::after {
         border-bottom-color: white;
         bottom: calc(100% - 1px);
       }
     }
-    &.position-left{
+    &.position-left {
       transform: translateX(-100%);
       margin-left: -10px;
-      &::before,&::after{
-        border-right: none;
-        transform: translateY(-50%);
-        top: 50%;
-      }
-      &::before{
-        border-left-color: black;
+      &::before,
+      &::after {
+        top: calc(50% - 5px);
         left: 100%;
+        border-left-color: black;
       }
-      &::after{
+      &::after {
         border-left-color: white;
         left: calc(100% - 1px);
       }
     }
-    &.position-right{
+    &.position-right {
       margin-left: 10px;
-      &::before,&::after{
-        border-left: none;
-        transform: translateY(-50%);
-        top: 50%;
-      }
-      &::before{
-        border-right-color: black;
+      &::before,
+      &::after {
+        top: calc(50% - 5px);
         right: 100%;
+        border-right-color: black;
       }
-      &::after{
+      &::after {
         border-right-color: white;
         right: calc(100% - 1px);
       }
     }
-
-  }
-  .buttonWrapper{
-    display: inline-block;
   }
 </style>
---
+++ tests/unit/popover.spec.js
@@ -0,0 +1,65 @@
+import { mount } from '@vue/test-utils'
+import Popover from '../../src/popover.vue'
+import sinonChai from 'sinon-chai'
+import sinon from 'sinon'
+import chai,{expect} from 'chai'
+chai.use(sinonChai)
+
+describe('测试Popover组件',() => {
+  it('存在', () => {
+    expect(Popover).to.be.exist
+  })
+
+  it('可以设置position',() => {
+    const wrapper = mount(Popover,{
+      propsData: {
+        position: 'bottom'
+      }
+    })
+    wrapper.find('.trigger').trigger('click')
+    expect(wrapper.find('.content-wrapper').classes()).to.includes('position-bottom')
+  })
+
+  it('可以接收visible属性',() => {
+    const wrapper = mount(Popover,{
+      propsData: {
+        visible: true
+      }
+    })
+    let contentWrapper = wrapper.find('.content-wrapper')
+    expect(contentWrapper).to.be.exist
+  })
+
+  xit('可以接收title属性',() => {
+    const wrapper = mount(Popover,{
+      propsData: {
+        title: '标题'
+      }
+    })
+    let title = wrapper.find('.title')
+    expect(title.textContent).to.equal('标题')
+  })
+
+  xit('可以接收content属性',() => {
+    const wrapper = mount(Popover,{
+      propsData: {
+        content: '内容'
+      }
+    })
+    let content = wrapper.find('.content')
+    expect(content.textContent).to.equal('内容')
+  })
+
+  xit('可以接收width属性',() => {
+    const wrapper = mount(Popover,{
+      propsData: {
+        width: '150'
+      }
+    })
+    let contentWrapper = wrapper.find('.content-wrapper')
+    let width = window.getComputedStyle(contentWrapper).width
+    expect(width).to.equal('150px')
+  })
+})
+
+
